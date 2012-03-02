a310c0fa9c2b7d9cc039c86d66d79107d36f6612
unknown <Artur@Artur-Komputer.(none)>
2012-03-02 12:54:15 +0100

    control rotation by timer

--- MainWindow.cs
+++ MainWindow.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
 
 namespace Project {
 
@@ -14,10 +16,25 @@ namespace Project {
     public partial class MainForm : Form
     {
         private RenderView renderView1;
-        private TextBox textBox1;
+        private TextBox timeBox;
         private Button button1;
+        private int timeFromBox = 30;
         //private RenderView view;
+        private System.Timers.Timer t = null;
 
+        private void setupTimer (object sender, System.EventArgs e) {
+                try{
+                    timeFromBox = Convert.ToInt32(timeBox.Text);
+                } catch (Exception er) {
+                    timeFromBox = 30;
+                }
+
+                if (timeFromBox < 3)
+                {
+                    timeFromBox = 30;
+                }
+                startTimer(timeFromBox);
+        }
 		public MainForm()
 		{
             InitializeComponent();
@@ -31,11 +48,47 @@ namespace Project {
 		//	this.Show();
 		}
 
+
+        public void startTimer(int dueTime)  {
+
+            if (t != null)
+            {
+                t.Stop();
+            }
+            //if (t == null) {
+                t = new System.Timers.Timer(dueTime/100.0);
+                //t = new Timer(new TimerCallback(TimerProc));
+            //}
+            //t.Interval = 1000.0;
+            t.Elapsed += new ElapsedEventHandler(ProcessTimerEvent);
+            
+            // timer.Elapsed += new ElapsedEventHandler(ProcessTimerEvent);
+
+            t.AutoReset = true;
+            t.Start();
+        }
+
+        private void ProcessTimerEvent(Object obj, ElapsedEventArgs e)
+        {
+          //  Thread demoThread = new Thread(new ThreadStart(this.ThreadProcUnsafe));
+         //   demoThread.Start();
+
+           
+            renderView1.Invoke((MethodInvoker)delegate(){
+                   renderView1.Refresh();
+             });
+            
+        }
+
+        //private void ThreadProcUnsafe() {
+        //   renderView1.Refresh();
+       // }
+
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
             this.renderView1 = new Project.RenderView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timeBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -46,6 +99,7 @@ namespace Project {
             this.button1.TabIndex = 0;
             this.button1.Text = "Set parameter";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.setupTimer);
             // 
             // renderView1
             // 
@@ -55,17 +109,17 @@ namespace Project {
             this.renderView1.TabIndex = 2;
             this.renderView1.Text = "renderView1";
             // 
-            // textBox1
+            // timeBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(359, 20);
-            this.textBox1.TabIndex = 3;
+            this.timeBox.Location = new System.Drawing.Point(12, 14);
+            this.timeBox.Name = "timeBox";
+            this.timeBox.Size = new System.Drawing.Size(359, 20);
+            this.timeBox.TabIndex = 3;
             // 
             // MainForm
             // 
             this.ClientSize = new System.Drawing.Size(484, 370);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.timeBox);
             this.Controls.Add(this.renderView1);
             this.Controls.Add(this.button1);
             this.Name = "MainForm";
